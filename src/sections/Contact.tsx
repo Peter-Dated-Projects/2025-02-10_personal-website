f6717b4aaf90e4c4eb7c61462867828c3e8237be
Peter Zhang <pezhang@blackberry.com>
2025-02-10 16:39:40 -0500

    initial

---
+++ src/sections/Contact.tsx
@@ -0,0 +1,19 @@
+
+import styles from './Contact.module.css'
+
+
+
+export default function ContactSection() {
+
+
+
+    return (
+        <div className={styles.container}>
+            <h1>Contact Me</h1>
+            <div className={styles.contact_grid}>
+                <p>Contact Information</p>
+                <p>Visualization of my Resume!</p>
+            </div>
+        </div>
+    );
+}
